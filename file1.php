e31af68adb0d1cae8d7bb6cb6876f35e30072296
Roman <roma.feshchenko.92@bk.ru>
2025-04-16 22:22:44 +0300

    Remove property

--- file1.php
+++ file1.php
@@ -1,17 +1,11 @@
 <?php
 class File1
 {
-    /**
-     * @param mixed $name
-     */
     public function setName($name): void
     {
         $this->name = $name;
     }
 
-    /**
-     * @return mixed
-     */
     public function getName()
     {
         return $this->name;
