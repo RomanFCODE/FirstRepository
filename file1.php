332e0fea1a74ae4b4f76fcf3c0bda86a15dab1c3
Roman <roma.feshchenko.92@bk.ru>
2025-04-16 14:45:22 +0300

    change file1.php

--- file1.php
+++ file1.php
@@ -1,1 +1,20 @@
 <?php
+class File1
+{
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+public $name;
+}
