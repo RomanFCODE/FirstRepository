2056ace49a2427cb931b41d790a56947e3260d55
Roman <roma.feshchenko.92@bk.ru>
2025-04-21 15:19:39 +0300

    Changed file1 step 2

--- file1.php
+++ file1.php
@@ -1,14 +1,27 @@
 <?php
 class File1
 {
+    public $surname = '';
+
+    public $name = '';
+
+
+    public function setSurname(string $surname): void
+    {
+        $this->surname = $surname;
+    }
+
+    public function getSurname(): string
+    {
+        return $this->surname;
+    }
+
     public function setName($name): void
     {
         $this->name = $name;
     }
-
     public function getName()
     {
         return $this->name;
     }
-public $name = '';
 }
