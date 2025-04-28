fca54def23e6c79625c823548e4ee4f85907e9eb
Roman <roma.feshchenko.92@bk.ru>
2025-04-28 17:33:20 +0300

    change file add get set age user 1

--- file1.php
+++ file1.php
@@ -7,7 +7,15 @@ class File1
 
     private int $age = 18;
 
+    public function getAge(): int
+    {
+        return $this->age;
+    }
 
+    public function setAge(int $age): void
+    {
+        $this->age = $age;
+    }
     public function setSurname(string $surname): void
     {
         $this->surname = $surname;
